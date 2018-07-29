41fe75e00067955486a4dd506d2c1e9f0f254821
_ivanC <aintivanc@icloud.com>
2018-07-29 21:42:04 +0800

    bugfix

--- ICFoundation.podspec
+++ ICFoundation.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ICFoundation'
-  s.version          = '0.2.1'
+  s.version          = '0.2.2'
   s.summary          = 'Utilities for system-classes extensions'
 
 # This description is used to generate tags and improve search results.
--- ICFoundation/Classes/UIScreen+Utilities.swift
+++ ICFoundation/Classes/UIScreen+Utilities.swift
@@ -8,15 +8,13 @@
 
 import UIKit
 
-import UIKit
-
 extension ICKit where Base == UIScreen {
     
-    static func isIPhoneX() -> Bool {
-        return fabs(UIScreen.main.bounds.height - 812) < 0.01
+    public func isIPhoneX() -> Bool {
+        return fabs(self.base.bounds.height - 812) < 0.01
     }
     
-    static func deviceBottomMarginHeight() -> CGFloat {
+    public func deviceBottomMarginHeight() -> CGFloat {
         return self.isIPhoneX() ? 34 : 0
     }
 }
